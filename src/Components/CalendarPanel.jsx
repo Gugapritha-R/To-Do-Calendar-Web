21886d6465a2f7f49f5461ce67334c5e41c6c4b7
Gugapritha-R <gugapritharao1803@gmail.com>
2024-06-20 23:59:31 +0530

    updated task and calendar panel

--- src/Components/CalendarPanel.jsx
+++ src/Components/CalendarPanel.jsx
@@ -5,6 +5,42 @@ const CalendarPanel = () => {
   return (
     <div className='calendar'>
 
+        <div className="calendar-panel">
+
+            <div className="header">
+
+            <p className='calendar-month'>June</p>
+            <p className='calendar-year'>2024</p>
+            
+
+            <div className="calendar-change">
+
+            <box-icon name='chevron-left' color='#b0adad' size='48px' ></box-icon>
+            <box-icon name='chevron-right' color='#b0adad' size='48px'></box-icon>
+
+
+            </div>
+
+
+            </div>
+
+
+            <div className="calendar-content">
+                
+                <div className="days">
+                    <p className='day-name'>Mo</p>
+                    <p className='day-name'>Tu</p>
+                    <p className='day-name'>We</p>
+                    <p className='day-name'>Th</p>
+                    <p className='day-name'>Fr</p>
+                    <p className='day-name'>Sa</p>
+                    <p className='day-name'>Su</p>
+                    
+                </div>
+            </div>
+            
+
+        </div>
     </div>
   )
 }
--- src/Components/TaskPanel.jsx
+++ src/Components/TaskPanel.jsx
@@ -4,23 +4,47 @@ import './Task.css';
 const TaskPanel = () => {
   return (
     <div className='taskpanel'>
+        {/*Date Frame */}
         <div className="dmy-frame">
 
         <div className="current-date">
         20
         </div>
+        
         <div className='month-year'>
+
         <div className="current-month">
         June
-      </div>
-      <div className="current-year">
+        </div>
+        <div className="current-year">
         2024
-      </div>
         </div>
 
-        
-      
-    </div>
+        </div>
+
+        </div>
+
+        {/* Today's Deadlines frame */}
+        <div className="today">
+            <p className='today-tasks'>Today's Deadlines</p>
+            <ul className='tasks'>
+                <li className='task-container'>Complete Frontend Challenge</li>
+                <li className='task-container'>Complete UI UX Challenge</li>
+            </ul>
+        </div>
+
+
+         {/* Upcoming Deadlines frame */}
+
+         <div className="upcoming">
+            <p className='upcoming-tasks'>Upcoming Deadlines</p>
+            <ul className='tasks'>
+                <li className='upcoming-task-container'>Do Something</li>
+                <li className='upcoming-task-container'>Finish Personal Project</li>
+            </ul>
+         </div>
+
+
      
 
     </div>
